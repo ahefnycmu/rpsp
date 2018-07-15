075ba2c7ea13aff68ded7b8d6da363e8d587f8c5
Zita Marinho <zmarinho@cmu.edu>
2018-07-15 09:33:48 +0200

    add deleted adam

--- rpsp/policy_opt/SGD_opt.py
+++ rpsp/policy_opt/SGD_opt.py
@@ -147,4 +147,53 @@ def adadelta(error, params, learning_rate=1e-2, rho=0.9, epsilon=1e-6, all_grads
         updates.append((p, p - learning_rate * g));
     return updates;
 
+
+# ADAM:
+def adam(loss, all_params, learning_rate=0.0002, beta1=0.1, beta2=0.001,
+         epsilon=1e-8, gamma=1 - 1e-8, clip_bounds=[], all_grads=[]):
+    """
+    ADAM gradient update.
+    @param loss: loss function
+    @param all_params: list of parameters to update
+    @param learning_rate: learning rate of descent direction
+    @param beta1: gradient coefficient in [0,1] if high give more importance to current vs. previous
+    @param beta2: variance coefficient in [0,1] if high give more importance to current vs. previous
+    @param epsilon: smoothing coefficient
+    @param gamma: time decay coefficient [0,1]
+    @param clip_bounds: gradient clip bounds [low,high]
+    @param all_grads: list of pre-computed gradients if len=0 will compute
+    @return: list of theano parameter updates (variable, updated_value)
+    """
+    updates = []
+    if len(all_grads) == 0:
+        cost = loss
+        if len(clip_bounds) == 2:
+            cost = theano.gradient.grad_clip(loss, clip_bounds[0], clip_bounds[1])
+        all_grads = T.grad(cost=cost, wrt=all_params);
+
+    i = theano.shared(np.float32(1))
+    i_t = i + 1.
+    fix1 = 1. - (1. - beta1) ** i_t
+    fix2 = 1. - (1. - beta2) ** i_t
+    # beta1_t = 1.-(1.- beta1)*gamma**(i_t-1)   # ADDED
+    learning_rate_t = learning_rate * (T.sqrt(fix2) / fix1)
+    for param_i, g in zip(all_params, all_grads):
+        param_i_value = param_i.get_value()
+        assert not np.isnan(param_i_value).any(), 'param is nan before update adam'
+        m = theano.shared(
+            np.zeros(param_i_value.shape, dtype=theano.config.floatX), name='adam_m_%s' % param_i.name)
+        v = theano.shared(np.zeros(param_i_value.shape, dtype=theano.config.floatX), name='adam_v_%s' % param_i.name)
+        # m_t = ((1. - beta1_t) * g) + (beta1_t * m) # CHANGED from b_t to use beta1_t goes from 0.1 to 1.0 over time
+        m_t = ((beta1) * g) + ((1. - beta1) * m)
+        v_t = (beta2 * g ** 2) + ((1. - beta2) * v)
+        g_t = m_t / (T.sqrt(T.abs_(v_t)) + epsilon)
+
+        param_i_t = param_i - (learning_rate_t * g_t)
+        updates.append((m, m_t))
+        updates.append((v, v_t))
+        updates.append((param_i, param_i_t))
+    updates.append((i, i_t))
+    return updates
+
+
 optimizers = {'adam': adam, 'RMSProp': RMSProp, 'adadelta': adadelta, 'adagrad': adagrad, 'sgd': sgd}
